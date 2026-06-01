eca9cbd28744062bb3bd6f8f06534e5f2b1bfa62
tudouyu-0903 <m15870616327@163.com>
2026-06-01 16:52:11 +0800

    基础AI交互实现和测试

--- src/main/java/com/cxy/aiprreview/app/PrReviewApp.java
+++ src/main/java/com/cxy/aiprreview/app/PrReviewApp.java
@@ -1,5 +1,9 @@
 package com.cxy.aiprreview.app;
+import com.cxy.aiprreview.advisor.MyLoggerAdvisor;
+import com.cxy.aiprreview.excption.BusinessException;
+import com.cxy.aiprreview.excption.ErrorCode;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -14,12 +18,6 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 @Component
 public class PrReviewApp {
     private final ChatClient chatClient;
-
-    public PrReviewApp(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder
-                .defaultSystem(SYSTEM_PROMPT)
-                .build();
-    }
     private static final String SYSTEM_PROMPT = """
             你是一个资深的 Java 架构师和技术专家。请对以下代码改动（Code Diff）进行严格的代码审查（Code Review）。
                             请重点关注以下几点：
@@ -32,16 +30,37 @@ public class PrReviewApp {
                             以下是需要审查的代码改动：
                             {diff}
             """;
-    public String reviewCode(ChatModel dashscopeChatModel,String codeDiff) {
+    public PrReviewApp(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MyLoggerAdvisor()
+                )
+                .build();
+    }
+
+    /***
+     * AI审查代码
+     * @param codeDiff 代码改动
+     * @return AI恢复的建议
+     */
+    public String reviewCode(String codeDiff) {
+        //1.判断改动是否为空
+        if (codeDiff == null || codeDiff.trim().isEmpty()) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "代码未改动");
+        }
         // 2. 使用 Spring AI 的 PromptTemplate 动态渲染参数
         PromptTemplate promptTemplate = new PromptTemplate(SYSTEM_PROMPT);
         Prompt prompt = promptTemplate.create(Map.of("diff", codeDiff));
 
         // 3. 调用大模型并获取文本结果
-        ChatResponse chatResponse = chatClient.prompt()
+        ChatResponse chatResponse = chatClient.prompt(prompt)
                 .system(SYSTEM_PROMPT )
                 .call()
                 .chatResponse();
+        if (chatResponse == null || chatResponse.getResult() == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "大模型返回结果为空");
+        }
         String content = chatResponse.getResult().getOutput().getText();
         return content;
     }
--- src/test/java/com/cxy/aiprreview/AiPrReviewApplicationTests.java
+++ src/test/java/com/cxy/aiprreview/AiPrReviewApplicationTests.java
@@ -1,6 +1,9 @@
 package com.cxy.aiprreview;
 
+import com.cxy.aiprreview.app.PrReviewApp;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,31 @@ class AiPrReviewApplicationTests {
     @Test
     void contextLoads() {
     }
-
+    @Resource
+    private PrReviewApp prReviewApp;
+    @Test
+    void reviewCode() {
+        String codeDiff = """
+                 package Hash;
+                
+                 import java.util.HashMap;
+                 import java.util.Map;
+                
+                 public class Test001 {
+                     public int[] twoSum(int[] nums, int target) {
+                         Map<Integer,Integer> map=new HashMap<>();
+                         for(int i=0;i<nums.length;i++){
+                             if(map.containsKey(target-nums[i])){
+                                 return new int[]{map.get(target-nums[i]),i};
+                             }
+                             map.put(nums[i],i);
+                         }
+                         throw new IllegalArgumentException("No two sum solution");
+                     }
+                 }
+                
+                """;
+        String content = prReviewApp.reviewCode(codeDiff);
+        System.out.println(content);
+    }
 }
